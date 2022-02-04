98428c1f35dbc1c5f9b78c7a98c66408b0fa7fc0
Ravi Teja Kesanam <kesanam.ravi@gmail.com>
2022-02-04 13:03:33 +1100

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
+import Header from './components/Header'
 import ProfileList from './components/ProfileList/ProfileList';
 
 function App() {
   return (
     <div className="App">
       
+      <Header />
+
       <ProfileList />
 
 
